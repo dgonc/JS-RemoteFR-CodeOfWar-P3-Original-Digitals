92980ee1e99371c17fef094f993b8090c2221c55
dgonc <d.goncalves89000@gmail.com>
2024-08-29 17:19:32 +0200

    :card_file_box: create table user, admin and movie in schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,29 @@
 create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
-  password varchar(255) not null
+  password varchar(255) not null,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+create table admin (
+  id int primary key auto_increment not null,
+  email varchar(255) not null unique,
+  password varchar(255) not null,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL
 );
+
+
+create table movie (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  duration INT NOT NULL,
+  synopsis TEXT NOT NULL,
+  date DATE NOT NULL,
+  classification INT NOT NULL,
+  picture VARCHAR(255),
+  admin_id INT NOT NULL,
+  Foreign Key (admin_id) REFERENCES admin(id)
+);
+
