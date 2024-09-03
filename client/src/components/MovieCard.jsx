7684eb307b73698d1f8210f3269e28cd195d9d5f
dgonc <d.goncalves89000@gmail.com>
2024-09-03 17:55:43 +0200

    :sparkles: add video in MovieCard

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
+import ReactPlayer from "react-player";
 
 export default function MovieCard({ movie }) {
   return (
     <div key={movie.id}>
+      <ReactPlayer controls url={movie.URL} />
       <h2>{movie.title}</h2>
       <div>
         {movie.duration} mins, -{movie.classification} ans
