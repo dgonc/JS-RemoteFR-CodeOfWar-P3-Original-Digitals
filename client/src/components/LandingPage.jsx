165747f85e28020132c17d3040c837aa1a154af5
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-09-04 11:38:24 +0200

    :memo: Update LandingPage component with additional content

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,7 +1,24 @@
 
 function LandingPage() {
     return (
-        <h1>youfilm</h1>
+        <div className="landing-container">
+            <header>
+                <div>YOUFLIM</div>
+                <div className="search-area">
+                    <span className="search-icon">🔎</span>
+                    <input type="text" className="search-bar" />
+                </div>
+            </header>
+            <section className="attract-section">
+                <img src="https://img-4.linternaute.com/uwngHQt8ik4-hjzRznv8FLvbBu8=/1500x/smart/a0fb5b2acf2b4d83844b9d90eaa4a0f3/ccmcms-linternaute/11240996.jpg" alt="Affiche du film Avengers" />
+                <p>Films et séries en ilimité.</p>
+                <p>Retrouvez vos shows et franchises favorites.</p>
+            </section>
+            <section className="buttons">
+                <button type="button">Débuter l'expérience !</button>
+                <button type="button">Pas sûr ? Testez notre partie gratuite.</button>
+            </section>
+        </div>
     );
 };
 
