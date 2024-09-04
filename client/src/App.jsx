b04dcceb748233ee171f5a78a38e432f7a9b3166
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-04 12:04:42 +0200

    :lipstick: add styling for userform component and sign in page, add nunito font local folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 import "./App.css";
+import "./styles/Userform.css";
 
 function App() {
   return (
--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -3,15 +3,26 @@ import { Form } from "react-router-dom"
 function UserForm() {
     return (
         <Form method="post">
+            <section className="form-fields">
+            <div className="form-group">
             <label htmlFor="email">Email</label>{" "}
-            <input type="text" id="email" name="email" /><br/>
+            <input type="text" id="email" name="email" />
+            </div>
+            <div className="form-group">
             <label htmlFor="password">Password</label>{" "}
-            <input type="text" id="password" name="password" /><br/>
+            <input type="text" id="password" name="password" />
+            </div>
+            <div className="form-group">
             <label htmlFor="firstname">Firstname</label>{" "}
-            <input type="text" id="firstname" name="firstname" /><br/>
+            <input type="text" id="firstname" name="firstname" />
+            </div>
+            <div className="form-group">
             <label htmlFor="lastname">Lastname</label>{" "}
-            <input type="text" id="lastname" name="lastname" /><br/>
-            <button type="submit">Ajouter</button>
+            <input type="text" id="lastname" name="lastname" />
+            </div>
+            <button type="submit">Sign Up</button>
+            <p className="sign-in-text">Already have an account ? Sign in</p>
+            </section>
         </Form>
     );
 }
