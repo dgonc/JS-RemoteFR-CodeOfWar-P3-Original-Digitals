346e0926c48ab63ffcd2c2b8eb53a88fef5918ef
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-19 17:24:39 +0200

    remove logout timer

--- client/src/pages/Logout.jsx
+++ client/src/pages/Logout.jsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Logout() {
-  const navigate = useNavigate();
-  const [counter, setCounter] = useState(3);
-
-  useEffect(() => {
-    if (counter === 0) {
-      navigate("/");
-    }
-    const timer =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-
-    return () => clearInterval(timer);
-  }, [counter, navigate]);
-
   return (
-    <>
+    <section className="logout-window">
       <p className="logout-text">You are now logged out.</p>
-      <p className="logout-timer">Redirect in : {counter} seconds</p>
-    </>
+      <Link to="/landing" className="go-back-button"> Go back to Landing page</Link>
+    </section>
   );
 }
 
