369855b3c2f11d989b8d9c3c5e6471af2fb13974
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-25 16:39:14 +0200

    create watchlist button to add and remove movies, add logic to reload watchlist when there is a change

--- client/src/pages/Watchlist.jsx
+++ client/src/pages/Watchlist.jsx
@@ -1,12 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useRevalidator } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
 export default function Watchlist() {
   const movies = useLoaderData();
+  const { revalidate } = useRevalidator();
   return (
     <div>
       {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} revalidate={revalidate} />
       ))}
     </div>
   );
