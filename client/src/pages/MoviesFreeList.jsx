2a34dc689b1b7f49639d0dba7de3187c929b793f
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-09-25 11:29:22 +0200

    :rocket: Add free movies feature with a dedicated route and page

---
+++ client/src/pages/MoviesFreeList.jsx
@@ -0,0 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+import "../styles/Movielist.css";
+
+export default function MoviesFreeList() {
+    const freeMovies = useLoaderData();
+
+    return (
+        <div className="card-container">
+            {freeMovies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+        </div>
+    );
+}
