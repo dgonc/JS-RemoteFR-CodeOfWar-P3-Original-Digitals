0f6c25466e1e4dad0dcfe70bfe5d9e5cd6bc0364
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-25 11:00:27 +0200

    new routes organization in router.js

--- server/app/router.js
+++ server/app/router.js
@@ -6,8 +6,6 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Import item-related actions
-
 const movieActions = require("./controllers/movieActions");
 const userActions = require("./controllers/userActions");
 const authActions = require("./controllers/authActions");
@@ -16,48 +14,35 @@ const auth = require("./services/auth");
 const middleware = require("./services/middleware");
 const authAdminActions = require("./controllers/authAdminActions");
 
-// route to get a list of movies
-router.get("/movies", movieActions.browse);
-router.get("/movies/categories", movieActions.readByCategories);
-router.get("/movies/:id", movieActions.read);
-router.get("/movies/search/:title", movieActions.readByTitle);
+// Public actions (no token necessary)
+router.post("/sign", middleware.verifyFields, auth.hashPassword, userActions.add);
+router.post("/login", authActions.verifyEmail, authActions.verifyPassword, auth.createToken, authActions.login);
+
+// Movies routes for users
+router.get("/movies", auth.verifyToken, movieActions.browse);
+router.get("/movies/categories", auth.verifyToken, movieActions.readByCategories);
+router.get("/movies/:id", auth.verifyToken, movieActions.read);
+router.get("/movies/search/:title", auth.verifyToken, movieActions.readByTitle);
+router.get("/categories", auth.verifyToken, categoryActions.browse);
+router.get("/categories/:id", auth.verifyToken, categoryActions.read);
+
+// Movies routes for admins
 router.post("/movies/add", movieActions.add);
 router.put("/movies/:id", movieActions.edit);
 
-// route to get a list of categories
-router.get("/categories", categoryActions.browse);
-router.get("/categories/:id", categoryActions.read);
-
-// routes for user related actions
+// Routes for user-related actions
 router.get("/checkauth", auth.verifyToken, auth.isConnected);
 router.get("/users", userActions.browse);
 router.get("/user/:id", userActions.read);
 router.put("/user/:id", userActions.edit);
 router.delete("/user/:id", userActions.destroy);
-router.post(
-  "/sign",
-  middleware.verifyFields,
-  auth.hashPassword,
-  userActions.add
-);
-router.post(
-  "/login",
-  authActions.verifyEmail,
-  authActions.verifyPassword,
-  auth.createToken,
-  authActions.login
-);
+
 router.get("/logout", auth.deleteToken);
 
 // routes for admin related actions
 router.get("/admins/checkauth", auth.verifyToken, auth.isConnected);
 router.get("/admins", authAdminActions.browseAdmin);
-router.post(
-  "/admins/sign",
-  middleware.verifyFields,
-  auth.hashPassword,
-  authAdminActions.add
-);
+router.post("/admins/sign", middleware.verifyFields, auth.hashPassword, authAdminActions.add);
 
 /* ************************************************************************* */
 
