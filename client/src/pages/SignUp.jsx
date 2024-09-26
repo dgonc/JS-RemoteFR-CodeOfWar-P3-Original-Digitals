708eb3f41a8d2ec99fcd82a2f4d95ad5ade70c42
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-26 16:41:26 +0200

    :lipstick: add desktop css to sign up page, and fix css issues, remove password strength fields when nothing is typed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,9 @@
-import { Link } from "react-router-dom";
 import UserForm from "../components/UserForm";
 
 function SignUp() {
   return (
     <section className="sign-up-page">
       <div className="sign-up-header">
-        <Link to="/landing" className="go-back-button">
-          Go back
-        </Link>
         <h1 className="sign-up-text">Sign Up !</h1>
       </div>
       <UserForm />
