21e5ffdff9c18f14742f4966ba6c6ba202728a56
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-26 10:51:26 +0200

    :lock: router.js heavy rework to protect all routes with token verification

--- server/app/router.js
+++ server/app/router.js
@@ -20,16 +20,19 @@ router.post("/sign", middleware.verifyFields, auth.hashPassword, userActions.add
 router.post("/login", authActions.verifyEmail, authActions.verifyPassword, auth.createToken, authActions.login);
 router.get("/movies/free", movieActions.browseFree);
 
+// all routes below require a token
+router.use(auth.verifyToken);
+
 // Movies routes for users
-router.get("/movies", auth.verifyToken, movieActions.browse);
-router.get("/movies/categories", auth.verifyToken, movieActions.readByCategories);
-router.get("/movies/:id", auth.verifyToken, movieActions.read);
-router.get("/movies/search/:title", auth.verifyToken, movieActions.readByTitle);
-router.get("/categories", auth.verifyToken, categoryActions.browse);
-router.get("/categories/:id", auth.verifyToken, categoryActions.read);
-router.get("/watchlist", auth.verifyToken, watchlistActions.read);
-router.post("/watchlist", auth.verifyToken, watchlistActions.add);
-router.delete("/watchlist", auth.verifyToken, watchlistActions.destroy);
+router.get("/movies", movieActions.browse);
+router.get("/movies/categories", movieActions.readByCategories);
+router.get("/movies/:id", movieActions.read);
+router.get("/movies/search/:title", movieActions.readByTitle);
+router.get("/categories", categoryActions.browse);
+router.get("/categories/:id", categoryActions.read);
+router.get("/watchlist", watchlistActions.read);
+router.post("/watchlist", watchlistActions.add);
+router.delete("/watchlist", watchlistActions.destroy);
 
 // Movies routes for admins
 router.post("/movies/add", movieActions.add);
@@ -37,19 +40,17 @@ router.put("/movies/:id", movieActions.edit);
 router.delete("/movies/:id", movieActions.destroy);
 
 // routes for user related actions
-router.get("/checkauth", auth.verifyToken, auth.isConnected);
+router.get("/checkauth", auth.isConnected);
 router.get("/users", userActions.browse);
 router.get("/user/:id", userActions.read);
 router.put("/user/:id", userActions.edit);
 router.delete("/user/:id", userActions.destroy);
-
 router.get("/logout", auth.deleteToken);
 
 // routes for admin related actions
-router.get("/admins/checkauth", auth.verifyToken, auth.isConnected);
+router.get("/admins/checkauth", auth.isConnected);
 router.get("/admins", authAdminActions.browseAdmin);
 router.post("/admins/sign", middleware.verifyFields, auth.hashPassword, authAdminActions.add);
-
 
 /* ************************************************************************* */
 
