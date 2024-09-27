a98fbe09b0b88da27415d97d9074e36f17490d35
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-27 17:47:15 +0200

    add server side upload middleware

--- server/app/services/middleware.js
+++ server/app/services/middleware.js
@@ -1,11 +1,15 @@
 const Joi = require("joi");
+const { v4: uuidv4 } = require("uuid");
+const multer = require("multer");
+
+const path = require("path");
 
 const verifyFields = (req, res, next) => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
     firstname: Joi.string().required(),
-    lastname: Joi.string().required()
+    lastname: Joi.string().required(),
   });
   const result = schema.validate(req.body);
 
@@ -16,4 +20,21 @@ const verifyFields = (req, res, next) => {
   }
 };
 
-module.exports = { verifyFields };
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "./public/uploads");
+  },
+  filename(req, file, cb) {
+    const id = uuidv4();
+    const movieName = `${id}${path.extname(file.originalname)}`;
+    req.body.movie.title = movieName;
+    cb(null, movieName);
+  },
+});
+
+const uploadMovie = (req, res, next) => {
+  const upload = multer({ storage });
+  return upload.single("movie")(req, res, next);
+};
+
+module.exports = { verifyFields, uploadMovie };
