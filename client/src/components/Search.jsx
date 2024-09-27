83649d67adc277bab0dcd34c2d82df3a02962626
Christian Colesnicov-Rouvellac <chris.colesnicov@gmail.com>
2024-09-27 16:41:41 +0200

    additional styling

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,49 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import { getCategories } from "../services/request";
 
 export default function Search({ toggleSearch }) {
   const [search, setSearch] = useState("");
-  const [categories, setCategories] = useState([]);
-  const [select, setSelect] = useState("Title");
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
   };
 
-  useEffect(() => {
-    async function fetchCategories() {
-      try {
-        const data = await getCategories();
-        setCategories(data);
-      } catch (error) {
-        console.error("Error in categories list", error);
-      }
-    }
-    fetchCategories();
-  }, []);
-
   return (
     <div className="search-container">
-      <select
-        value={select}
-        name="select"
-        id="select"
-        onChange={(e) => setSelect(e.target.value)}
-      >
-        <option value="title">Title</option>
-        <option value="categories">Category</option>
-      </select>
-      {select === "categories" ? (
-        <select name="select" id="select-category">
-          {categories.map((category) => (
-            <option key={category.id}>{category.type}</option>
-          ))}
-        </select>
-      ) : (
-        ""
-      )}
       <input className="search-input" type="text" onChange={handleSearch} />
       <Link
         className="search-action"
